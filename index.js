4587afbcfd252033e51c24fbf6ab3295fe63d128
el-sam <samurai.h@yahoo.com>
2023-09-22 10:45:10 +0800

    complete writeCards function

--- index.js
+++ index.js
@@ -6,23 +6,49 @@
 //     debugger;
 // }
 
-// Add gift array
-const gifts = ["teddy bear","drone","doll","lego"];
+// // Add gift array
+// const gifts = ["teddy bear","drone","doll","lego"];
 
-// Create a function to wrap the gifts one by one
-function wrapGifts(gifts) {
-    // Loop each element by for loop and apply action on each item
-    for (let i = 0; i < gifts.length; i++){
-        //let gift = gitfs[i];
-        console.log(`Wrapped ${gifts[i]} and added a bow!`);
-        // Add breaking point
-        debugger;
-    }
-    /*Below is the 'return' keyword. comment it out when complete debugging. 
-    but 'return' line don't have to be commented out while debuggin. 
-    */ 
-    return gifts;
+// // Create a function to wrap the gifts one by one
+// function wrapGifts(gifts) {
+//     // Loop each element by for loop and apply action on each item
+//     for (let i = 0; i < gifts.length; i++){
+//         //let gift = gitfs[i];
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         // Add breaking point
+//         debugger;
+//     }
+//     /*Below is the 'return' keyword. comment it out when complete debugging. 
+//     but 'return' line don't have to be commented out while debuggin. 
+//     */ 
+//     return gifts;
     
-} 
+// } 
 
-wrapGifts(gifts);
+// wrapGifts(gifts);
+
+// Create function to generate thanks cards
+function writeCards(arr,event) {
+   
+   // Create an empty array
+   const friends = []; 
+   let count = 0; 
+   let msg = "";
+   
+    // Loop the passed-in array of receivers, add thanks meesage for them
+    for (let i = 0; i < arr.length; i++){
+        count += 1;
+        msg = msg + `Thank you ${arr[i]} for your wonderful ${event} gift!`;
+        debugger; 
+        
+        
+    }
+    friends.push(msg);
+    console.log(friends);  
+    //let newFriends = [...friends,`msg`];
+    return friends;
+    
+
+}
+
+writeCards(["ali","tony","alex"],"birthday");
